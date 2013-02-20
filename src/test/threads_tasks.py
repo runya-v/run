833950c54bf9d032c9c9a28b045442a75a2ee2ec
Velichko Rostislav <velichko.rostislav@alphaopen.com>
2013-02-20 16:07:00 +0400

    Разработал пример многопоточной обработки одноимённых сообщений.

--- src/test/threads_tasks.py
+++ src/test/threads_tasks.py
@@ -4,6 +4,7 @@ import sys
 import Queue
 import threading
 import random
+import time
 
 
 class Help:
@@ -15,54 +16,106 @@ class Help:
 
 class MessageGenerator(threading.Thread):
 
-    def __init__(self, work_queue, max_msg_id=10):
+    def __init__(self, lock, msg_queue_dict, max_msg_id):
         super(MessageGenerator, self).__init__()
-        self.work_queue = work_queue
+
+        self.stop_process = False
+        self.lock = lock
+        self.msg_queue_dict = msg_queue_dict
         self.max_msg_id = max_msg_id
+
+    def stop(self):
+        with self.lock:
+            self.stop_process = True
 
     def run(self):
         try:
-            while True:
-                self.work_queue.put(random.randint(0, self.max_msg_id))
+            while not self.stop_process:
+                sequance = random.randint(1, self.max_msg_id)
+                
+                while sequance != 0:
+                    sequance = sequance-1
+                    msg_id = random.randint(0, self.max_msg_id)
+                    
+                    with self.lock:
+                        list_len = len(self.msg_queue_dict)
+                        queue = Queue.Queue()
+                        
+                        if msg_id in self.msg_queue_dict:
+                            queue = self.msg_queue_dict[msg_id]
+                        else:
+                            self.msg_queue_dict[msg_id] = queue
+                            
+                        queue.put(random.random())
         finally:
             pass
 
 
 class Worker(threading.Thread):
 
-    def __init__(self, work_queue):
+    def __init__(self, lock, msg_queue_dict):
         super(Worker, self).__init__()
-        self.work_queue = work_queue
-        self.msg_list_list = [[]]
+
+        self.stop_process = False
+        self.lock = lock
+        self.msg_queue_dict = msg_queue_dict
+
+    def stop(self):
+        with self.lock:
+            self.stop_process = True
 
     def run(self):
         try:
-            while True:
-                msg_id = self.work_queue.get()
-                self.msg_list_list[msg_id]
-                
+            while not self.stop_process:
+                with self.lock:
+                    if len(self.msg_queue_dict):
+                        msg_id = self.msg_queue_dict.keys()[0]
+                        queue = self.msg_queue_dict[msg_id]
+                        
+                        print '>'
+                        
+                        while not queue.empty():
+                            print 'msg: '+ str(msg_id) + " content: " + str(queue.get())
+                            
+                        self.msg_queue_dict.pop(msg_id)
         finally:
             pass
 
 
 class ServiceModel:
     
-    def __int__(self, threads_num, max_msg_id):
-        work_queue = Queue.Queue()
+    def __init__(self, threads_num, max_msg_id=10):
+        lock = threading.Lock()
+        msg_queue_dict = {}
 
-        MessageGenerator(work_queue, max_msg_id).start()
+        msg_gen = MessageGenerator(lock, msg_queue_dict, max_msg_id)
+        msg_gen.start()
+        
+        workers = []
 
         for i in range(threads_num):
-            Worker(work_queue).start()
+            wkr = Worker(lock, msg_queue_dict)
+            workers.append(wkr)
+            wkr.start()
+            
+        time.sleep(20)
+
+        msg_gen.stop()
+
+        for i in range(len(workers)):
+            workers[i].stop()
 
 
 def main(argv=sys.argv):
 
-    if len(argv) <= 2:
+    if len(argv) <= 1:
         Help()
         return 1
-    
-    ServiceModel(int(argv[1]), int(argv[2]))
+
+    if len(argv) == 3:
+        ServiceModel(int(argv[1]), int(argv[2]))
+    else:
+        ServiceModel(int(argv[1]))
 
     return 0    
 
